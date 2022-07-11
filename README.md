869ab7b8628eaa7ed000479af9b368a5b532dbb6
Michael Berg-Mohnicke <michael.berg@zalf.de>
2022-07-11 15:32:44 +0200

    updated readme and starter script

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Code examples for 2022 EMS object capability paper
 
-This repository contains the some working and complete code examples for the (as of yet) unpublished EMS paper on object capabilities in our ZALF model and simulation infrastructure. These examples are not necessarily supposed to present the most secure and best way to design an API, rather to support the paper and show working examples. We try to add different implementations in different languages and maybe subsequently add more complete and closer to the real world examples. The currently incomplete real infrastructure code can be found https://github.com/zalf-rpm/mas-infrastructure.
+This repository contains some working and complete code examples for the (as of yet) unpublished EMS paper on object capabilities in our ZALF model and simulation infrastructure. These examples are not necessarily supposed to present the most secure and best way to design an API, rather to support the paper and show working examples. We try to add different implementations in different languages and maybe subsequently add more complete and closer to the real world examples. The currently incomplete real infrastructure code can be found https://github.com/zalf-rpm/mas-infrastructure.
 
 ## Use & Compilation
 Different languages have different needs. Except for Python all languages need the schema files in the capnp folder to be processed by the Capn'n Proto schema compiler to generate the actual languages code. The following sections will try to list what's needed to run and/or compile the according languages code.
@@ -9,28 +9,63 @@ The different implementations should be interchangeable.
 
 ### Python
 
-requirements
+__requirements__
+
 - Python3
 - install pycapnp via pip
 
+* run Python example
+
+    python revokable_forwarder_example_aio.py a=py b=py c=py m=py
+
 ### C#
 
-requirements
+__requirements__
 - dotnet 6.0
 
 ### Go
 
-requirements
+__requirements__
 
 ### C++
 
-requirements
+__requirements__
+
 - cmake
+- VCPKG
+- git
+- Python3 (for running the starter script)
+
+__installation__
+
+* checkout this repository and dependencies
+
+    git clone https://github.com/zalf-rpm/ems_ocaps_paper_2022.git
+    git clone https://github.com/Microsoft/vcpkg.git
+
+* build vcpkg
+
+    cd vcpkg
+    ./bootstrap-vcpkg.sh
+
+* build Cap'n Proto
+
+    ./vcpkg install capnproto:x64-linux
+
+* build cpp code
+
+    cd ems_ocaps_paper_2022
+    sh init_cmake_linux_debug.sh
+    cd _cmake_debug
+    make
+
+* run cpp example
+
+    python revokable_forwarder_example_aio.py a=cpp b=cpp c=cpp m=cpp
 
 
 ## Examples
 
-1. classic Alice, Bob and Carol interaction
-2. additional to 1. Alice sends to Bob a revokable capability to Carol
+1. classic Alice, Bob and Carol interaction with Alice sends to Bob a revokable capability to Carol
 
 
--- revokable_forwarder_example_aio.py
+++ revokable_forwarder_example_aio.py
@@ -15,25 +15,57 @@
 # Landscape Systems Analysis at the ZALF.
 # Copyright (C: Leibniz Centre for Agricultural Landscape Research (ZALF)
 
-import time
 import subprocess as sp
+import sys
+import time
 
-alice_process = sp.Popen(["cpp/_cmake_debug/revokable_forwarder_example", "--actor=alice"])
-#alice_process = sp.Popen(["csharp/bin/Debug/net6.0/revokable_forwarder_example", "actor=alice"])
-#alice_process = sp.Popen(["python", "python/alice.py", "use_asyncio=true"])
+def update_config(config, argv, print_config=False, allow_new_keys=False):
+    if len(argv) > 1:
+        for arg in argv[1:]:
+            k, v = arg.split("=")
+            if not allow_new_keys and k in config:
+                config[k] = v.lower() == "true" if v.lower() in ["true", "false"] else v 
+        if print_config:
+            print(config)
 
-#bob_process = sp.Popen(["cpp/_cmake_debug/revokable_forwarder_example", "--actor=bob"])
-bob_process = sp.Popen(["csharp/bin/Debug/net6.0/revokable_forwarder_example", "actor=bob"])
-#bob_process = sp.Popen(["python", "python/bob.py", "use_asyncio=true"])
+config = {
+    "a": "cpp", # cpp | c# | (go) | py
+    "b": "cpp", # cpp | c# | (go) | py
+    "c": "cpp", # cpp | c# | (go) | py
+    "m": "cpp", # cpp | c# | (go) | py
+    "py_use_asyncio": "true",
+}
+update_config(config, sys.argv, print_config=False)
 
-#carol_process = sp.Popen(["cpp/_cmake_debug/revokable_forwarder_example", "--actor=carol"])
-#carol_process = sp.Popen(["csharp/bin/Debug/net6.0/revokable_forwarder_example", "actor=carol"])
-carol_process = sp.Popen(["python", "python/carol.py", "use_asyncio=true"])
+if config["a"] == "cpp":
+    alice_process = sp.Popen(["cpp/_cmake_debug/revokable_forwarder_example", "--actor=alice"])
+elif config["a"] == "c#":
+    alice_process = sp.Popen(["csharp/bin/Debug/net6.0/revokable_forwarder_example", "actor=alice"])
+elif config["a"] == "py":
+    alice_process = sp.Popen(["python", "python/alice.py", "use_asyncio="+config["py_use_asyncio"]])
+
+if config["b"] == "cpp":
+    bob_process = sp.Popen(["cpp/_cmake_debug/revokable_forwarder_example", "--actor=bob"])
+elif config["b"] == "c#":
+    bob_process = sp.Popen(["csharp/bin/Debug/net6.0/revokable_forwarder_example", "actor=bob"])
+elif config["b"] == "py":
+    bob_process = sp.Popen(["python", "python/bob.py", "use_asyncio="+config["py_use_asyncio"]])
+
+if config["c"] == "cpp":
+    carol_process = sp.Popen(["cpp/_cmake_debug/revokable_forwarder_example", "--actor=carol"])
+elif config["c"] == "c#":
+    carol_process = sp.Popen(["csharp/bin/Debug/net6.0/revokable_forwarder_example", "actor=carol"])
+elif config["c"] == "py":
+    carol_process = sp.Popen(["python", "python/carol.py", "use_asyncio="+config["py_use_asyncio"]])
 time.sleep(2)
 
-#main_process = sp.Popen(["cpp/_cmake_debug/revokable_forwarder_example", "--actor=main"])
-main_process = sp.Popen(["csharp/bin/Debug/net6.0/revokable_forwarder_example", "actor=main"])
-#main_process = sp.Popen(["python", "python/revokable_forwarder_example.py", "actor=main", "use_asyncio=false"])
+if config["m"] == "cpp":
+    main_process = sp.Popen(["cpp/_cmake_debug/revokable_forwarder_example", "--actor=main"])
+elif config["m"] == "c#":
+    main_process = sp.Popen(["csharp/bin/Debug/net6.0/revokable_forwarder_example", "actor=main"])
+elif config["m"] == "py":
+    main_process = sp.Popen(["python", "python/revokable_forwarder_example.py", "actor=main", "use_asyncio="+config["py_use_asyncio"]])
+
 main_process.wait()
 
 alice_process.terminate()
